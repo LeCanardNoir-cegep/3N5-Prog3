bb7f83b7cc3d722ba6fe2417ee89172d84bfe533
Bruno Pouliot <2168516@cegepmontpetit.ca>
2022-08-23 16:48:23 -0400

    Init: Hello world

---
+++ HelloWorld/src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+        System.out.println("What the heck!!");
+    }
+}
